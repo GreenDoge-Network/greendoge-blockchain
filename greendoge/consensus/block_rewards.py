4512b6da281a78e9a27a53b75818ea32beed2c0b
lionethan <83430349+lionethan@users.noreply.github.com>
2021-07-10 16:55:12 +0800

    Update block_rewards.py

--- greendoge/consensus/block_rewards.py
+++ greendoge/consensus/block_rewards.py
@@ -51,7 +51,7 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
     """
     if height == 0:
         return uint64(int((1 / 8) * 21000000 * _mojo_per_greendoge))
-    elif height < (3 / 12) * _blocks_per_year: #Early participation rewards
+    elif height < (1 / 12) * _blocks_per_year: #Early participation rewards
         return uint64(int((1 / 8) * 100 * _mojo_per_greendoge))
     elif height < 1 * _blocks_per_year:
         return uint64(int((1 / 8) * 50 * _mojo_per_greendoge))
