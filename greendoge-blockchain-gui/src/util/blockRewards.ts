23e716e7ef0b7405cdd884a1ef4fc1a7205b781e
lionethan <83430349+lionethan@users.noreply.github.com>
2021-07-11 12:30:38 +0800

    Optimization

--- greendoge-blockchain-gui/src/util/blockRewards.ts
+++ greendoge-blockchain-gui/src/util/blockRewards.ts
@@ -7,38 +7,43 @@ export function calculatePoolReward(height: number): Big {
   if (height === 0) {
     return MOJO_PER_GREENDOGE.times(21000000).times(7 / 8);
   }
+  if (height < 3 / 12 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_GREENDOGE.times(100).times(7 / 8);
   if (height < 3 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(2 * 50).times(7 / 8);
+    return MOJO_PER_GREENDOGE.times(20).times(7 / 8);
   }
   if (height < 6 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(1 * 50).times(7 / 8);
+    return MOJO_PER_GREENDOGE.times(10).times(7 / 8);
   }
   if (height < 9 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(0.5 * 50).times(7 / 8);
+    return MOJO_PER_GREENDOGE.times(5).times(7 / 8);
   }
   if (height < 12 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(0.25 * 50).times(7 / 8);
+    return MOJO_PER_GREENDOGE.times(2.5).times(7 / 8);
   }
 
-  return MOJO_PER_GREENDOGE.times(0.125 * 50).times(7 / 8);
+  return MOJO_PER_GREENDOGE.times(1.25).times(7 / 8);
 }
 
 export function calculateBaseFarmerReward(height: number): Big {
   if (height === 0) {
     return MOJO_PER_GREENDOGE.times(21000000).times(1 / 8);
   }
+  if (height < 3 / 12 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_GREENDOGE.times(100).times(1 / 8);
+  }
   if (height < 3 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(2 * 50).times(1 / 8);
+    return MOJO_PER_GREENDOGE.times(20).times(1 / 8);
   }
   if (height < 6 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(1 * 50).times(1 / 8);
+    return MOJO_PER_GREENDOGE.times(10).times(1 / 8);
   }
   if (height < 9 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(0.5 * 50).times(1 / 8);
+    return MOJO_PER_GREENDOGE.times(5).times(1 / 8);
   }
   if (height < 12 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_GREENDOGE.times(0.25 * 50).times(1 / 8);
+    return MOJO_PER_GREENDOGE.times(2.5).times(1 / 8);
   }
 
-  return MOJO_PER_GREENDOGE.times(0.125 * 50).times(1 / 8);
+  return MOJO_PER_GREENDOGE.times(1.25).times(1 / 8);
 }
