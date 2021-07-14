e29cae758cb7c8a83601c811697f966cacb76879
lionethan <83430349+lionethan@users.noreply.github.com>
2021-07-14 09:43:22 +0800

    Reduce the halving time.

--- greendoge-blockchain-gui/src/util/blockRewards.ts
+++ greendoge-blockchain-gui/src/util/blockRewards.ts
@@ -10,16 +10,16 @@ export function calculatePoolReward(height: number): Big {
   if (height < (1 / 12) * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(100).times(7 / 8);
   }
-  if (height < 3 * BLOCKS_PER_YEAR) {
+  if (height < 2 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(20).times(7 / 8);
   }
-  if (height < 6 * BLOCKS_PER_YEAR) {
+  if (height < 4 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(10).times(7 / 8);
   }
-  if (height < 9 * BLOCKS_PER_YEAR) {
+  if (height < 6 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(5).times(7 / 8);
   }
-  if (height < 12 * BLOCKS_PER_YEAR) {
+  if (height < 8 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(2.5).times(7 / 8);
   }
 
@@ -33,16 +33,16 @@ export function calculateBaseFarmerReward(height: number): Big {
   if (height < (1 / 12) * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(100).times(1 / 8);
   }
-  if (height < 3 * BLOCKS_PER_YEAR) {
+  if (height < 2 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(20).times(1 / 8);
   }
-  if (height < 6 * BLOCKS_PER_YEAR) {
+  if (height < 4 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(10).times(1 / 8);
   }
-  if (height < 9 * BLOCKS_PER_YEAR) {
+  if (height < 6 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(5).times(1 / 8);
   }
-  if (height < 12 * BLOCKS_PER_YEAR) {
+  if (height < 8 * BLOCKS_PER_YEAR) {
     return DOG_PER_GREENDOGE.times(2.5).times(1 / 8);
   }
 
--- greendoge/consensus/block_rewards.py
+++ greendoge/consensus/block_rewards.py
@@ -18,13 +18,13 @@ def calculate_pool_reward(height: uint32) -> uint64:
         return uint64(int((7 / 8) * 21000000 * _dog_per_greendoge))
     elif height < (1 / 12) * _blocks_per_year:
         return uint64(int((7 / 8) * 100 * _dog_per_greendoge)) #Reward for early participants in the community.
-    elif height < 3 * _blocks_per_year:
+    elif height < 2 * _blocks_per_year:
         return uint64(int((7 / 8) * 20 * _dog_per_greendoge))
-    elif height < 6 * _blocks_per_year:
+    elif height < 4 * _blocks_per_year:
         return uint64(int((7 / 8) * 10 * _dog_per_greendoge))
-    elif height < 9 * _blocks_per_year:
+    elif height < 6 * _blocks_per_year:
         return uint64(int((7 / 8) * 5 * _dog_per_greendoge))
-    elif height < 12 * _blocks_per_year:
+    elif height < 8 * _blocks_per_year:
         return uint64(int((7 / 8) * 2.5 * _dog_per_greendoge))
     else:
         return uint64(int((7 / 8) * 1.25 * _dog_per_greendoge))
@@ -43,13 +43,13 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
         return uint64(int((1 / 8) * 21000000 * _dog_per_greendoge))
     elif height < (1 / 12) * _blocks_per_year:
         return uint64(int((1 / 8) * 100 * _dog_per_greendoge)) #Reward for early participants in the community.
-    elif height < 3 * _blocks_per_year:
+    elif height < 2 * _blocks_per_year:
         return uint64(int((1 / 8) * 20 * _dog_per_greendoge))
-    elif height < 6 * _blocks_per_year:
+    elif height < 4 * _blocks_per_year:
         return uint64(int((1 / 8) * 10 * _dog_per_greendoge))
-    elif height < 9 * _blocks_per_year:
+    elif height < 6 * _blocks_per_year:
         return uint64(int((1 / 8) * 5 * _dog_per_greendoge))
-    elif height < 12 * _blocks_per_year:
+    elif height < 8 * _blocks_per_year:
         return uint64(int((1 / 8) * 2.5 * _dog_per_greendoge))
     else:
         return uint64(int((1 / 8) * 1.25 * _dog_per_greendoge))
