43739c844804c527d4f46dd9b7cf886fe96bf6c7
HiveProject2021 <jwang123@nyit.edu>
2021-07-26 20:25:41 -0700

    Update farm.py

--- greendoge/cmds/farm.py
+++ greendoge/cmds/farm.py
@@ -53,18 +53,6 @@ def summary_cmd(rpc_port: int, wallet_rpc_port: int, harvester_rpc_port: int, fa
 
     asyncio.run(summary(rpc_port, wallet_rpc_port, harvester_rpc_port, farmer_rpc_port))
 
-@farm_cmd.command("uploadfarmerdata", short_help="Upload the farm summary and challenges data to community.chivescoin.org, and you can query the data in this site.")
-@click.option(
-    "-p",
-    "--rpc-port",
-    help=(
-        "Set the port where the Full Node is hosting the RPC interface. "
-        "See the rpc_port under full_node in config.yaml"
-    ),
-    type=int,
-    default=None,
-    show_default=True,
-)
 
 @farm_cmd.command("challenges", short_help="Show the latest challenges")
 @click.option(
@@ -88,7 +76,49 @@ def challenges_cmd(farmer_rpc_port: int, limit: int) -> None:
     import asyncio
 
     asyncio.run(challenges(farmer_rpc_port, limit))
-    
+   
+
+@farm_cmd.command("uploadfarmerdata", short_help="Upload the farm summary and challenges data to greendoge.chivescoin.org, and you can query the data in this site.")
+@click.option(
+    "-p",
+    "--rpc-port",
+    help=(
+        "Set the port where the Full Node is hosting the RPC interface. "
+        "See the rpc_port under full_node in config.yaml"
+    ),
+    type=int,
+    default=None,
+    show_default=True,
+)
+@click.option(
+    "-wp",
+    "--wallet-rpc-port",
+    help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
+    type=int,
+    default=None,
+    show_default=True,
+)
+@click.option(
+    "-hp",
+    "--harvester-rpc-port",
+    help=(
+        "Set the port where the Harvester is hosting the RPC interface"
+        "See the rpc_port under harvester in config.yaml"
+    ),
+    type=int,
+    default=None,
+    show_default=True,
+)
+@click.option(
+    "-fp",
+    "--farmer-rpc-port",
+    help=(
+        "Set the port where the Farmer is hosting the RPC interface. " "See the rpc_port under farmer in config.yaml"
+    ),
+    type=int,
+    default=None,
+    show_default=True,
+)
 def uploadfarmerdata_cmd(rpc_port: int, wallet_rpc_port: int, harvester_rpc_port: int, farmer_rpc_port: int) -> None:
     print("Ready to upload harvester data to greendoge.chivescoin.org...")
     from .farm_funcs import summary,challenges,uploadfarmerdata
@@ -102,7 +132,7 @@ def uploadfarmerdata_cmd(rpc_port: int, wallet_rpc_port: int, harvester_rpc_port
         FarmerSatusJson = json.dumps(FarmerSatus)
         print('------------------------------------------------------------------')
         print(datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S"))
-        print(f"Upload the farm summary and challenges data to community.chivescoin.org, and you can query the data in this site.")
+        print(f"Upload the farm summary and challenges data to greendoge.chivescoin.org, and you can query the data in this site.")
         content = requests.post('https://greendoge.chivescoin.org/farmerinfor/uploaddata.php', data={'FarmerSatus':FarmerSatusJson})
         try:
             content = content.json()
