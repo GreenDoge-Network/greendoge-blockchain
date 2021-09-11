8fc5e4d58bcb992caeb57d2c472d5c1a107799dc
lionethan <83430349+lionethan@users.noreply.github.com>
2021-09-11 18:32:44 +0800

    Please refer to the post-launch GreenDoge emission schedule in the whitepaper version 1.0.0

--- greendoge/consensus/block_rewards.py
+++ greendoge/consensus/block_rewards.py
@@ -12,22 +12,28 @@ def calculate_pool_reward(height: uint32) -> uint64:
     These halving events will not be hit at the exact times
     (3 years, etc), due to fluctuations in difficulty. They will likely come early, if the network space and VDF
     rates increase continuously.
+
+    For the post-launch GreenDoge emission schedule, please refer to the whitepaper.
     """
 
     if height == 0:
         return uint64(int((7 / 8) * 21000000 * _dog_per_greendoge))
     elif height < (1 / 12) * _blocks_per_year:
         return uint64(int((7 / 8) * 100 * _dog_per_greendoge))  # Reward for early participants in the community.
-    elif height < 2 * _blocks_per_year:
+    elif height < (6 / 12) * _blocks_per_year:
         return uint64(int((7 / 8) * 20 * _dog_per_greendoge))
-    elif height < 4 * _blocks_per_year:
+    elif height < 1 * _blocks_per_year:
         return uint64(int((7 / 8) * 10 * _dog_per_greendoge))
-    elif height < 6 * _blocks_per_year:
+    elif height < 2 * _blocks_per_year:
         return uint64(int((7 / 8) * 5 * _dog_per_greendoge))
-    elif height < 8 * _blocks_per_year:
+    elif height < 4 * _blocks_per_year:
         return uint64(int((7 / 8) * 2.5 * _dog_per_greendoge))
-    else:
+    elif height < 6 * _blocks_per_year:
         return uint64(int((7 / 8) * 1.25 * _dog_per_greendoge))
+    elif height < 8 * _blocks_per_year:
+        return uint64(int((7 / 8) * 0.625 * _dog_per_greendoge))
+    else:
+        return uint64(int((7 / 8) * 0.3125 * _dog_per_greendoge))
 
 
 def calculate_base_farmer_reward(height: uint32) -> uint64:
@@ -38,18 +44,24 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
     Returns the coinbase reward at a certain block height. These halving events will not be hit at the exact times
     (3 years, etc), due to fluctuations in difficulty. They will likely come early, if the network space and VDF
     rates increase continuously.
+
+    For the post-launch GreenDoge emission schedule, please refer to the whitepaper.
     """
     if height == 0:
         return uint64(int((1 / 8) * 21000000 * _dog_per_greendoge))
     elif height < (1 / 12) * _blocks_per_year:
         return uint64(int((1 / 8) * 100 * _dog_per_greendoge))  # Reward for early participants in the community.
-    elif height < 2 * _blocks_per_year:
+    elif height < (6 / 12) * _blocks_per_year:
         return uint64(int((1 / 8) * 20 * _dog_per_greendoge))
-    elif height < 4 * _blocks_per_year:
+    elif height < 1 * _blocks_per_year:
         return uint64(int((1 / 8) * 10 * _dog_per_greendoge))
-    elif height < 6 * _blocks_per_year:
+    elif height < 2 * _blocks_per_year:
         return uint64(int((1 / 8) * 5 * _dog_per_greendoge))
-    elif height < 8 * _blocks_per_year:
+    elif height < 4 * _blocks_per_year:
         return uint64(int((1 / 8) * 2.5 * _dog_per_greendoge))
-    else:
+    elif height < 6 * _blocks_per_year:
         return uint64(int((1 / 8) * 1.25 * _dog_per_greendoge))
+    elif height < 8 * _blocks_per_year:
+        return uint64(int((1 / 8) * 0.625 * _dog_per_greendoge))
+    else:
+        return uint64(int((1 / 8) * 0.3125 * _dog_per_greendoge))
